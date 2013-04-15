6aa51335acc5ca1453fcf0c1f90c98b73e26e412
priya <priya.rajan@bitsighttech.com>
2013-04-15 13:18:59 +0530

    test

--- TestCSVParser/src/main/java/com/csvParser/TestCSV.java
+++ TestCSVParser/src/main/java/com/csvParser/TestCSV.java
@@ -3,6 +3,7 @@ package com.csvParser;
 import java.io.IOException;
 
 import au.com.bytecode.opencsv.CSVParser;
+import au.com.bytecode.opencsv.CSVReader;
 
 public class TestCSV {
 
@@ -12,9 +13,7 @@ public class TestCSV {
 	 */
 	public static void main(String[] args) throws IOException
 	{
-		CSVParser csvParser = new CSVParser(',');
-		ss
-
+		CSVReader csvParser = new CSVParser(',');
 		String[] str = csvParser.parseLine("1,2,\"Bitsight Technologies,INC\"");
 		for(String s : str) {
 			System.out.println(s);
